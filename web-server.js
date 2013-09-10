5446f6aed6f434af42254af9bab937327730003d
sezar-gantous <sezar.gantous@outlook.com>
2013-09-10 12:12:38 -0400

    fix port so it works with heroku

--- web-server.js
+++ web-server.js
@@ -6,7 +6,7 @@ var util = require('util'),
     url = require('url'),
     events = require('events');
 
-var DEFAULT_PORT = 8000;
+var DEFAULT_PORT = process.env.PORT || 8000;
 
 function main(argv) {
   new HttpServer({
@@ -40,7 +40,7 @@ function HttpServer(handlers) {
 
 HttpServer.prototype.start = function(port) {
   this.port = port;
-  this.server.listen(port);
+  this.server.listen(process.env.PORT || port);
   util.puts('Http Server running at http://localhost:' + port + '/');
 };
 
