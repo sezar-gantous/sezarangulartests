ab0f309130d830fbdf5b738573684f0f2dd02fea
sezar-gantous <sezar.gantous@outlook.com>
2013-09-15 10:11:02 -0400

    trying to make the last commit work(home page is fixed) -6

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ angular.module('phonecat', ['phonecatFilters', 'phonecatServices']).
   $routeProvider.
       when('/phones', {templateUrl: 'partials/phone-list.html',   controller: PhoneListCtrl}).
       when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
-      when('/todoApp', {templateUrl: 'partials/todo-partial.html', controller: 'TodoCtrl'}).
+      when('/todoApp', {templateUrl: 'partials/todo-partial.html', controller: TodoCtrl}).
       when('/', {templateUrl: 'partials/index-partial.html', controller: IndexCtrl}).
       otherwise({redirectTo: '/'});
 }]);
