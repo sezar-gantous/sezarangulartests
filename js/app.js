edc4116a609b199bdc30e932c1bd25b731c5a43b
sezar-gantous <sezar.gantous@outlook.com>
2013-09-15 09:47:21 -0400

    added a home page and menu bar to nav to the two apps..

--- js/app.js
+++ js/app.js
@@ -5,8 +5,9 @@
 angular.module('phonecat', ['phonecatFilters', 'phonecatServices']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
+      when('/index', {templateUrl: 'partials/index-partial.html',   }).
       when('/phones', {templateUrl: 'partials/phone-list.html',   controller: PhoneListCtrl}).
       when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
       when('/todoApp', {templateUrl: 'partials/todo-partial.html', controller: TodoCtrl}).
-      otherwise({redirectTo: '/phones'});
+      otherwise({redirectTo: '/index'});
 }]);
