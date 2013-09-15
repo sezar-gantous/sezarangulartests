6068f2b999ca6f648501b3bc319cdd6db722704c
sezar-gantous <sezar.gantous@outlook.com>
2013-09-15 10:00:13 -0400

    trying to make the last commit work -4

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 angular.module('phonecat', ['phonecatFilters', 'phonecatServices']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/', {templateUrl: 'partials/index-partial.html' controller: IndexCtrl}).
+      when('/', {templateUrl: 'partials/index-partial.html', controller: IndexCtrl}).
       when('/phones', {templateUrl: 'partials/phone-list.html',   controller: PhoneListCtrl}).
       when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
       when('/todoApp', {templateUrl: 'partials/todo-partial.html', controller: 'TodoCtrl'}).
